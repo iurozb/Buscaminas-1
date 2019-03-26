e061d1d9de78f6477e3f9a5e7ea52e8de6ea5fa9
alumno <alumno@aula1-06>
2019-03-26 16:29:14 +0100

    grimer

---
+++ Buscaminas/src/buscaminas/Casilla.java
@@ -0,0 +1,19 @@
+
+package buscaminas;
+
+
+public class Casilla {
+    
+    private boolean mina;
+    private boolean blanco;
+    private boolean bandera;
+    private boolean visible;
+    private int numero;
+
+    public Casilla() {
+    }
+
+    
+    
+    
+}
